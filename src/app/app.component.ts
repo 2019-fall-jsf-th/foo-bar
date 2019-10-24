26ddbbb590469dcb1f67129b90ffcf1bb5997166
tsteele <tsteele@madisoncollege.edu>
2019-10-24 18:49:28 -0500

    Initial commit...

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  //template: '<h1>FooBar</h1> <h3>{{ title }}</h3>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'foo-bar';
+}
