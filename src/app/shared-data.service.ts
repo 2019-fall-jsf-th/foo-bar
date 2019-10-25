f38ee71ebed444390e417c0b56f51ee237bddde9
tsteele <tsteele@madisoncollege.edu>
2019-10-24 20:00:14 -0500

    Replaced 'normal' property with getter and setter...

--- src/app/shared-data.service.ts
+++ src/app/shared-data.service.ts
@@ -7,5 +7,18 @@ export class SharedDataService {
 
   constructor() { }
 
-  data = 'Uninitialized';
+  // Normal property...
+  //data = 'Uninitialized';
+
+  private internalData = 'Uninitialized';
+
+  public get data() {
+    console.log('Getter');
+    return this.internalData;
+  }
+
+  public set data(value) {
+    console.log('Setter');
+    this.internalData = value;
+  }
 }
